fd373b03210085f6396f661cf33a73b3de59fedc
Steven <muthanjistephen34@gmail.com>
2023-12-01 14:13:03 +0300

    myfirst commit

---
+++ python1.py
@@ -0,0 +1,12 @@
+def perimeter (length, width):
+    return 2 * (length + width)
+print(perimeter(345, 768))
+
+def per(l ,w):
+    total = (l+w)*2
+    return total
+i = int(input("Enter length"))
+y = int(input("enter width"))
+
+output = per(i, y)
+print(output)
